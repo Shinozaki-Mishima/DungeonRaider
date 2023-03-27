3625b022d248e7b4c87db39cae05512e44daeb48
Shinozaki-Mishima <shivandavi56@gmail.com>
2023-03-27 11:34:59 -0400

    Began development on C++ components.
    Added a new character blueprint; BP_Player
    Added a C++ Actor Component

--- DungeonRaider/Intermediate/Build/Win64/UnrealEditor/Inc/DungeonRaider/DungeonRaider.init.gen.cpp
+++ DungeonRaider/Intermediate/Build/Win64/UnrealEditor/Inc/DungeonRaider/DungeonRaider.init.gen.cpp
@@ -23,13 +23,13 @@ void EmptyLinkFunctionForGeneratedCodeDungeonRaider_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x26390EF2,
-				0x7C1BE6C7,
+				0x07313B92,
+				0x58C7DC24,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_DungeonRaider.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_DungeonRaider.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonRaider(Z_Construct_UPackage__Script_DungeonRaider, TEXT("/Script/DungeonRaider"), Z_Registration_Info_UPackage__Script_DungeonRaider, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x26390EF2, 0x7C1BE6C7));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonRaider(Z_Construct_UPackage__Script_DungeonRaider, TEXT("/Script/DungeonRaider"), Z_Registration_Info_UPackage__Script_DungeonRaider, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x07313B92, 0x58C7DC24));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
